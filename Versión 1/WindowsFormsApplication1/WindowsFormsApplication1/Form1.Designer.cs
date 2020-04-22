f754f5449246f7e7c003b77d1a802ff77565d24f
CristinaPerez96 <perezordonezcristina@outlook.es>
2020-04-23 00:51:56 +0200

    Versión 2

--- Versión 1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ Versión 1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -45,11 +45,16 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.Conectados = new System.Windows.Forms.Button();
+            this.Cont = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Jugador = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // Registro
             // 
-            this.Registro.Location = new System.Drawing.Point(93, 253);
+            this.Registro.Location = new System.Drawing.Point(97, 223);
             this.Registro.Name = "Registro";
             this.Registro.Size = new System.Drawing.Size(103, 51);
             this.Registro.TabIndex = 0;
@@ -59,7 +64,7 @@
             // 
             // Sesion
             // 
-            this.Sesion.Location = new System.Drawing.Point(310, 253);
+            this.Sesion.Location = new System.Drawing.Point(265, 223);
             this.Sesion.Name = "Sesion";
             this.Sesion.Size = new System.Drawing.Size(103, 52);
             this.Sesion.TabIndex = 1;
@@ -69,14 +74,14 @@
             // 
             // user
             // 
-            this.user.Location = new System.Drawing.Point(222, 106);
+            this.user.Location = new System.Drawing.Point(177, 116);
             this.user.Name = "user";
             this.user.Size = new System.Drawing.Size(191, 22);
             this.user.TabIndex = 2;
             // 
             // contraseña
             // 
-            this.contraseña.Location = new System.Drawing.Point(222, 167);
+            this.contraseña.Location = new System.Drawing.Point(177, 177);
             this.contraseña.Name = "contraseña";
             this.contraseña.Size = new System.Drawing.Size(191, 22);
             this.contraseña.TabIndex = 3;
@@ -84,17 +89,16 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(90, 110);
+            this.label1.Location = new System.Drawing.Point(45, 120);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(57, 17);
             this.label1.TabIndex = 4;
             this.label1.Text = "Usuario";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // password
             // 
             this.password.AutoSize = true;
-            this.password.Location = new System.Drawing.Point(93, 171);
+            this.password.Location = new System.Drawing.Point(48, 181);
             this.password.Name = "password";
             this.password.Size = new System.Drawing.Size(68, 17);
             this.password.TabIndex = 5;
@@ -102,9 +106,9 @@
             // 
             // Desconectar
             // 
-            this.Desconectar.Location = new System.Drawing.Point(180, 353);
+            this.Desconectar.Location = new System.Drawing.Point(236, 28);
             this.Desconectar.Name = "Desconectar";
-            this.Desconectar.Size = new System.Drawing.Size(152, 61);
+            this.Desconectar.Size = new System.Drawing.Size(152, 47);
             this.Desconectar.TabIndex = 6;
             this.Desconectar.Text = "Desconectar";
             this.Desconectar.UseVisualStyleBackColor = true;
@@ -112,7 +116,7 @@
             // 
             // Conectar
             // 
-            this.Conectar.Location = new System.Drawing.Point(180, 24);
+            this.Conectar.Location = new System.Drawing.Point(48, 27);
             this.Conectar.Name = "Conectar";
             this.Conectar.Size = new System.Drawing.Size(152, 48);
             this.Conectar.TabIndex = 7;
@@ -123,40 +127,40 @@
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(521, 83);
+            this.radioButton1.Location = new System.Drawing.Point(456, 91);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(17, 16);
             this.radioButton1.TabIndex = 8;
             this.radioButton1.TabStop = true;
             this.radioButton1.UseVisualStyleBackColor = true;
-            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_Click);
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(521, 157);
+            this.radioButton2.Location = new System.Drawing.Point(456, 135);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(17, 16);
             this.radioButton2.TabIndex = 9;
             this.radioButton2.TabStop = true;
             this.radioButton2.UseVisualStyleBackColor = true;
-            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_Click);
             // 
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(521, 229);
+            this.radioButton3.Location = new System.Drawing.Point(456, 178);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(17, 16);
             this.radioButton3.TabIndex = 10;
             this.radioButton3.TabStop = true;
             this.radioButton3.UseVisualStyleBackColor = true;
-            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            this.radioButton3.Click += new System.EventHandler(this.radioButton3_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(544, 66);
+            this.label2.Location = new System.Drawing.Point(479, 74);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(187, 17);
             this.label2.TabIndex = 11;
@@ -165,7 +169,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(544, 83);
+            this.label3.Location = new System.Drawing.Point(479, 91);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(178, 17);
             this.label3.TabIndex = 12;
@@ -174,7 +178,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(545, 143);
+            this.label4.Location = new System.Drawing.Point(480, 121);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(150, 17);
             this.label4.TabIndex = 13;
@@ -183,7 +187,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(545, 160);
+            this.label5.Location = new System.Drawing.Point(480, 138);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(164, 17);
             this.label5.TabIndex = 14;
@@ -192,26 +196,67 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(544, 219);
+            this.label6.Location = new System.Drawing.Point(479, 168);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(187, 17);
+            this.label6.Size = new System.Drawing.Size(192, 17);
             this.label6.TabIndex = 15;
-            this.label6.Text = "Número de partidas jugadas";
+            this.label6.Text = "Número de partidas ganadas";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(544, 236);
+            this.label7.Location = new System.Drawing.Point(479, 185);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(105, 17);
             this.label7.TabIndex = 16;
             this.label7.Text = "por el \"usuario\"";
+            // 
+            // Conectados
+            // 
+            this.Conectados.Location = new System.Drawing.Point(421, 344);
+            this.Conectados.Name = "Conectados";
+            this.Conectados.Size = new System.Drawing.Size(163, 76);
+            this.Conectados.TabIndex = 18;
+            this.Conectados.Text = "Ver conectados";
+            this.Conectados.UseVisualStyleBackColor = true;
+            this.Conectados.Click += new System.EventHandler(this.Conectados_Click);
+            // 
+            // Cont
+            // 
+            this.Cont.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Cont.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cont.Location = new System.Drawing.Point(482, 244);
+            this.Cont.Name = "Cont";
+            this.Cont.Size = new System.Drawing.Size(146, 67);
+            this.Cont.TabIndex = 20;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Jugador});
+            this.dataGridView1.Location = new System.Drawing.Point(97, 303);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(271, 150);
+            this.dataGridView1.TabIndex = 21;
+            // 
+            // Jugador
+            // 
+            this.Jugador.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Jugador.HeaderText = "Jugador Conectado";
+            this.Jugador.Name = "Jugador";
+            this.Jugador.ReadOnly = true;
+            this.Jugador.Width = 143;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(734, 465);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Cont);
+            this.Controls.Add(this.Conectados);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -231,6 +276,7 @@
             this.Controls.Add(this.Registro);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -255,6 +301,10 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button Conectados;
+        private System.Windows.Forms.Label Cont;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Jugador;
     }
 }
 
--- Versión 1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ Versión 1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -8,57 +8,112 @@ using System.Text;
 using System.Windows.Forms;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
         Socket server;
+        Thread atender;
 
         public Form1()
         {
             InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false; //Para evitar problemas de Threads cruzados
+
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        //Función para atender las respuestas del servidor
+        private void AtenderServidor()
         {
+            while (true)
+            {
+                //Recibimos la respuesta del servidor
+                byte[] msg2 = new byte[80];
+                server.Receive(msg2);
+                string[] trozos = Encoding.ASCII.GetString(msg2).Split('/');
+                int codigo = Convert.ToInt32(trozos[0]);
+                string mensaje= trozos[1].Split('\0')[0];
 
+                switch (codigo)
+                {
+                    case 1: //Registra un usuario
+                        MessageBox.Show(mensaje);
+                        break;
+
+                    case 2: //Inicia sesión
+                        MessageBox.Show(mensaje);
+                        break;
+
+                    case 3: //Nombre del jugador que ganó en menor tiempo
+                        MessageBox.Show(mensaje);
+                        break;
+
+                    case 4: //Menor tiempo en ganar del usuario
+                        MessageBox.Show(mensaje);
+                        break;
+
+                    case 5: //Partidas ganadas por el usuario
+                        MessageBox.Show(mensaje);
+                        break;
+
+                    case 6: //Lista conectados
+                        int i = 0;
+                        int n;
+                        dataGridView1.Rows.Clear();
+                        dataGridView1.Refresh();
+                        int trozo = Int32.Parse(trozos[1]);
+                        if (mensaje == "0")
+                        {
+                            MessageBox.Show("No hay conectados");
+                        }
+                        else
+                        {
+                            while (i < trozo - 1)
+                            {
+                                n = dataGridView1.Rows.Add();
+                                i++;
+                            }
+                        }
+                        for (i = 0; i < trozo; i++)
+                        {
+                            dataGridView1.Rows[i].Cells[0].Value = trozos[i+2];
+                        }
+                        break;
+
+                    case 7: //Servicios realizados
+                        Cont.Text = mensaje;
+                        break;
+                }
+            }
         }
+
+
+
+        
 
         //Botón de registro de usuario
         private void Registro_Click(object sender, EventArgs e)
         {
 
-                // Quiere realizar un registro en la BBDD
-                string mensaje = "1/" + user.Text + "/" + contraseña.Text;
-                // Enviamos al servidor el user tecleado y la contraseña
-                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
-                server.Send(msg);
-
-                //Recibimos la respuesta del servidor
-                byte[] msg2 = new byte[80];
-                server.Receive(msg2);
-                mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
-                MessageBox.Show(mensaje);
-           
+            // Quiere realizar un registro en la BBDD
+            string mensaje = "1/" + user.Text + "/" + contraseña.Text;
+            // Enviamos al servidor el user tecleado y la contraseña
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
 
         }
 
         //Botón de inicio de sesión
         private void Sesion_Click(object sender, EventArgs e)
         {
-                // Quiere realizar un inicio de sesión
-                string mensaje = "2/" + user.Text + "/" + contraseña.Text;
-                // Enviamos al servidor el user tecleado y la contraseña
-                byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
-                server.Send(msg);
+            // Quiere realizar un inicio de sesión
+            string mensaje = "2/" + user.Text + "/" + contraseña.Text;
+            // Enviamos al servidor el user tecleado y la contraseña
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
 
-                //Recibimos la respuesta del servidor
-                byte[] msg2 = new byte[80];
-                server.Receive(msg2);
-                mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
-                MessageBox.Show(mensaje);
-            
         }
 
         //Desconectamos del servidor
@@ -99,61 +154,50 @@ namespace WindowsFormsApplication1
                 //Si hay excepcion imprimimos error y salimos del programa con return 
                 MessageBox.Show("No he podido conectar con el servidor");
                 return;
-            } 
+            }
+
+            //Pongo en marcha el thread que atenderá los mensajes del servidor
+            ThreadStart ts = delegate{ AtenderServidor(); };
+            atender = new Thread(ts);
+            atender.Start();
         }
 
         //Consulta el nombre del jugador que ganó en menor tiempo
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        private void radioButton1_Click(object sender, EventArgs e)
         {
             // Quiere realizar la consulta escogida
-            string mensaje = "3/" + user.Text ;
-            // Enviamos al servidor el user tecleado y la contraseña
+            string mensaje = "3/";
+            // Enviamos al servidor el user tecleado 
             byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
             server.Send(msg);
-
-            //Recibimos la respuesta del servidor
-            byte[] msg2 = new byte[80];
-            server.Receive(msg2);
-            mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
-            MessageBox.Show(mensaje);
         }
 
         //Consulta el mejor tiempo en el que ganó el usuario indicado en el user.TextBox
-        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        private void radioButton2_Click(object sender, EventArgs e)
         {
             // Quiere realizar la consulta escogida
             string mensaje = "4/" + user.Text;
-            // Enviamos al servidor el user tecleado y la contraseña
+            // Enviamos al servidor el user tecleado 
             byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
             server.Send(msg);
-
-            //Recibimos la respuesta del servidor
-            byte[] msg2 = new byte[80];
-            server.Receive(msg2);
-            mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
-            MessageBox.Show(mensaje);
         }
 
         //Consulta el número de partidas jugadas por el usuario indicado en el user.TextBox
-        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        private void radioButton3_Click(object sender, EventArgs e)
         {
             // Quiere realizar la consulta escogida
             string mensaje = "5/" + user.Text;
-            // Enviamos al servidor el user tecleado y la contraseña
+            // Enviamos al servidor el user tecleado 
             byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
             server.Send(msg);
-
-            //Recibimos la respuesta del servidor
-            byte[] msg2 = new byte[80];
-            server.Receive(msg2);
-            mensaje = Encoding.ASCII.GetString(msg2).Split('\0')[0];
-            MessageBox.Show(mensaje);
         }
 
-
-
-        
-
-        
+        private void Conectados_Click(object sender, EventArgs e)
+        {
+            // Quiere realizar la consulta escogida
+            string mensaje = "6/";
+            byte[] msg = System.Text.Encoding.ASCII.GetBytes(mensaje);
+            server.Send(msg);
+        }
     }
 }
